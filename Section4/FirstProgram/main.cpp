4270df1318bb3939972c7577fde5155dab1ebf95
mia373 <mint373@hotmail.com>
2026-01-13 23:36:56 -0800

    initial commit

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int favorite_number;
+
+    // Print the string literal to the console
+    std::cout << "Enter your favorite number between 1 and 100: ";
+
+    // Read the input from the user and store it in the favorite_number variable
+    std::cin >> favorite_number;
+
+    // Print the string literal to the console
+    std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+    
+    return 0;
+}
